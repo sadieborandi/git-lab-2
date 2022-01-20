8fc06eac16c19f214dca9f3cd945f8b75f9f2295
Sadie Borandi <sb618820@ohio.edu>
2022-01-19 20:21:48 -0500

    Hi

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Sadie Borandi>
+ *        Date: <01/19/2022>
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!"  << endl;
+    /*add code*/
+    return 0;
+}// main
